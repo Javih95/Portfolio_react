442541d35959e88fd8a6d920d99d58805819ba63
Javih <javi.aguirre111095@gmail.com>
2025-01-26 18:45:01 -0300

    agrague funcion de emails

--- vite-project/src/views/Contacto.jsx
+++ vite-project/src/views/Contacto.jsx
@@ -1,15 +1,36 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import "./Css/Contacto.css"
 
 export const Contacto = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_p5wtvjb', 'template_sjchv85', form.current, {
+        publicKey: 'w2N5IkWy2lpT_INmD',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <section id="contact" className="contact">
       <h2>Contacto</h2>
       <p>¿Quieres colaborar conmigo o tienes alguna consulta? ¡Escríbeme!</p>
-      <form className="contact-form">
-        <input type="text" placeholder="Tu Nombre" required />
-        <input type="email" placeholder="Tu Email" required />
-        <textarea placeholder="Tu Mensaje" rows="5" required></textarea>
-        <button type="submit">Enviar</button>
+      <form className="contact-form" ref={form} onSubmit={sendEmail}>
+        <input type="text" name="user_name" placeholder="Tu Nombre" required />
+        <input type="email" name="user_email" placeholder="Tu Email" required />
+        <textarea name="message" placeholder="Tu Mensaje" rows="5" required></textarea>
+        <button type="submit" value="Send">Enviar</button>
       </form>
       <div className="contact-links">
         <a
