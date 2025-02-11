b4b6101f4b84cdfc2543ce76aa37c1446e283250
Javih <javi.aguirre111095@gmail.com>
2025-02-10 22:16:19 -0300

    config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:["/Portfolio_react/"],
+  base:["https://javih95.github.io/Portfolio_react/"],
   plugins: [react()],
 })
